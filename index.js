4befb233fd7042108fe641aa26f8b5d09a575728
rohit prasain <rohit1@gmail.com>
2021-02-04 19:18:16 +0545

    form connected and data submitted to database

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-const express  = require('express');
-
+const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
 
+app.set('view engine' , 'ejs');
 
 // //import routes
 const authRoute = require('./routes/auth');
 
-// app.get('/register' , (req, res)=> { 
-//     res.send("register");
-// })
+dotenv.config();
+
+//connect to db
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true },
+    () => {
+        console.log("connection successful");
+    }
+);
+
+//middleware
+app.use (express.json());
+
+app.use(express.urlencoded({
+    extended: true
+  }))
+
+app.get('/register' , (req, res)=> { 
+    res.render('form');
+})
+
+
 //routes middlewares
-app.use('/api/user' , authRoute);
+app.use('/api/user', authRoute);
 
 
-app.listen(3000 , ()=> {
+app.listen(3000, () => {
     console.log("listening to port 3000");
 })
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const User = require('../model/User');
 
-router.get('/register' , (req , res)=> {
-    res.send("registeration");
+
+router.post('/register' , async (req , res)=> {
+    // res.send("registeration");
+
+    const user = new User ({
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password,
+    });
+
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err){
+        res.status(400).send(err);
+    }
 })
 
 
