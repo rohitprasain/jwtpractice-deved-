f837c115dcedd450266bd811778a804094468c0c
rohit prasain <rohit1@gmail.com>
2021-02-04 21:51:12 +0545

    checking done if the email already exists in the database

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,27 @@ const router = require('express').Router();
 const User = require('../model/User');
 
 
-router.post('/register' , async (req , res)=> {
-    // res.send("registeration");
 
+router.get('/haha' , (req, res , next) => {
+    res.send("haha");
+});
+
+
+
+
+router.post('/register' , async (req , res)=> {
+
+    //checking if the user already exists in the database
+
+    const emailExist = await User.findOne({
+        email : req.body.email
+    })
+
+    if(emailExist) { 
+        return res.status(400).send('email aleaready exists in the database');
+    }
+  
+    //creating new user
     const user = new User ({
         name : req.body.name,
         email : req.body.email,
